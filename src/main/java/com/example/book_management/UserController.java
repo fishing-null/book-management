a7cebff11752a10bc0ee5f85775233132faca4ad
MatouSakura <1134979484@qq.com>
2023-10-29 19:10:07 +0800

    后端实现用户登录接口

--- src/main/java/com/example/book_management/UserController.java
+++ src/main/java/com/example/book_management/UserController.java
@@ -1,2 +1,23 @@
-package com.example.book_management;public class UserController {
+package com.example.book_management;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/login")
+    public boolean login(String userName, String password, HttpSession session) {
+        if(!StringUtils.hasLength(userName) || !StringUtils.hasLength(password)){
+            return false;
+        }
+        if("admin".equals(userName) && "admin".equals(password)){
+            session.setAttribute("userName",userName);
+            return true;
+        }
+        return false;
+    }
 }
