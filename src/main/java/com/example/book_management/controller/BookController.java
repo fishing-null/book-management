2cbc80045c5c70318c27243b065601b928b6d77d
MatouSakura <1134979484@qq.com>
2023-10-30 22:25:09 +0800

    基于三层模型管理文件

--- src/main/java/com/example/book_management/controller/BookController.java
+++ src/main/java/com/example/book_management/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.example.book_management.controller;
 
+import com.example.book_management.dao.BookDao;
 import com.example.book_management.model.BookInfo;
+import com.example.book_management.service.BookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,30 +16,9 @@ import java.util.Random;
 public class BookController {
     @RequestMapping("/getBookList")
     public List<BookInfo> getBookList(){
-        List<BookInfo> bookInfos = mockData();
-        for(BookInfo bookInfo:bookInfos){
-            if(bookInfo.getStatus() == 1){
-                bookInfo.setStatusCN("可借阅");
-            }else{
-                bookInfo.setStatusCN("不可借阅");
-            }
-        }
+        BookService bookService = new BookService();
+        List<BookInfo> bookInfos = bookService.getBookList();
         return bookInfos;
     }
 
-    private List<BookInfo> mockData() {
-        List<BookInfo> bookInfos = new ArrayList<>(15);
-        for (int i = 0; i < 15; i++) {
-            BookInfo bookInfo = new BookInfo();
-            bookInfo.setId(1);
-            bookInfo.setBookName("图书" + i);
-            bookInfo.setAuthor("作者" + i);
-            bookInfo.setCount(new Random().nextInt(200));
-            bookInfo.setPrice(new BigDecimal(new Random().nextInt(100)));
-            bookInfo.setPublish("出版社" + i);
-            bookInfo.setStatus(i % 5 == 0 ? 2 : 1);
-            bookInfos.add(bookInfo);
-        }
-        return bookInfos;
-    }
 }
