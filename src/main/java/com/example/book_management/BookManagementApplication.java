6b03d24c305ba1a6203062d787c89fffd7a4f1ca
MatouSakura <1134979484@qq.com>
2024-02-27 14:08:39 +0800

    图书管理系统

--- src/main/java/com/example/book_management/BookManagementApplication.java
+++ src/main/java/com/example/book_management/BookManagementApplication.java
@@ -2,12 +2,15 @@ package com.example.book_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BookManagementApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookManagementApplication.class, args);
+
+        Applicationc application = SpringApplication.run(BookManagementApplication.class, args);
+
     }
 
 }
